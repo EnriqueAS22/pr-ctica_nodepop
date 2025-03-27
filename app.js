c8382722970fddc92daae44eb95d86d630e04b3f
Enrique Alba <enalsa2222@gmail.com>
2025-03-27 12:10:54 +0100

    feat: login/logout

--- app.js
+++ app.js
@@ -4,26 +4,32 @@ import ejs from 'ejs'
 import connectMongoose from './lib/connectMongoose.js'
 import * as homeController from './controllers/homeController.js'
 import * as loginController from './controllers/loginController.js'
+import * as sessionManager from './lib/sessionManager.js'
 
 await connectMongoose()
 console.log('Connected to MongoDB')
 
 const app = express()
 
-app.use(logger('dev'))
-app.use(express.urlencoded({ extended: false }))
-
 app.set('views', 'views')
 app.set('view engine', 'html')
 app.engine('html', ejs.__express )
+
 app.locals.appName= 'NodePop'
+
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
+
 
 /**
  * Aplication routes
  */
+app.use(sessionManager.middleware)
+app.use(sessionManager.useSessionInViews)
 app.get('/', homeController.index)
 app.get('/login', loginController.index)
 app.post('/login', loginController.postLogin)
-app.post('/tags_products', homeController.tagsProducts)
+app.get('/logout', loginController.logout)
+//app.post('/tags_products', homeController.tagsProducts)
 
 export default app
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,6 +2,7 @@ import User from '../models/Users.js'
 
 export function index(req, res, next) {
     res.locals.error = ''
+    res.locals.email = ''
     res.render('login')
 }
 
@@ -15,14 +16,27 @@ export async function postLogin(req, res, next) {
         // si no encuentro o no es la contraseña, devuelve Error
         if (!user || !(await user.comparePassword(password))) {
             res.locals.error = 'Invalid credentials'
+            res.locals.email = email
             res.render('login')
             return
         }
     
         // si es bueno todo -> redirect a la Home estando Logado
+        req.session.userId = user.id
+
         res.redirect('/')
 
     } catch (error) {
         next(error)
     }
+}
+
+export function logout(req, res, next) {
+    req.session.regenerate(err => {
+        if (err) {
+            next(err)
+            return
+        }
+        res.redirect('/')
+    })
 }
