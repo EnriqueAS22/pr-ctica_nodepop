ee99b042a02b9429aa77729ba8f679cb5e1c29de
Enrique Alba <enalsa2222@gmail.com>
2025-03-27 12:34:18 +0100

    feat: pagina nuevo producto

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import connectMongoose from './lib/connectMongoose.js'
 import * as homeController from './controllers/homeController.js'
 import * as loginController from './controllers/loginController.js'
 import * as sessionManager from './lib/sessionManager.js'
+import * as productsController from './controllers/productsController.js'
 
 await connectMongoose()
 console.log('Connected to MongoDB')
@@ -30,6 +31,7 @@ app.get('/', homeController.index)
 app.get('/login', loginController.index)
 app.post('/login', loginController.postLogin)
 app.get('/logout', loginController.logout)
+app.get('/products/new', productsController.index)
 //app.post('/tags_products', homeController.tagsProducts)
 
 export default app
