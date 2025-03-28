b4d96e9129457e60ecd75740795d5adb645d79a1
Enrique Alba <enalsa2222@gmail.com>
2025-03-28 14:33:23 +0100

    feat: faltan error

--- initDB.js
+++ initDB.js
@@ -22,6 +22,7 @@ if (answer !== 'y') {
  * INIT
  */
 await initUsers()
+await initProducts()
 await connection.close()
 
 /**
@@ -33,11 +34,73 @@ async function initUsers() {
 
     const insertResult = await User.insertMany([
         { email: 'paco@usuario.com', password: await User.hashPassword('1234') },
-        { email: 'kike@usuario.com', password: await User.hashPassword('2345') },
-        { email: 'manu@usuario.com', password: await User.hashPassword('3456') }
+        { email: 'kike@usuario.com', password: await User.hashPassword('1234') },
     ])
     
     console.log(`Insert ${insertResult.length} users.`)
+}
+
+async function initProducts() {
+	const result = await Product.deleteMany()
+	console.log(`${result.deletedCount} products deleted`)
+
+	const [paco, kike] = await Promise.all([
+		User.findOne({email: 'paco@usuario.com'}),
+		User.findOne({email: 'kike@usuario.com'})
+	])
+
+	const insertResult = await Product.insertMany([
+		{
+			name : "Air Jordan",
+			owner: kike._id,
+			price : 48,
+			image : "Air_Jordam.jpg",
+			tags : [ "Lifestyle" ]
+		},
+		{
+			name : "Caja de herramientas",
+			owner: paco._id,
+			price : 47,
+			image : "Caja_herramientas.jpg",
+			tags : [ "Work" ]
+		},
+		{
+			name : "Kawasaki Ninja",
+			owner: kike._id,
+			price : 13000,
+			image : "Kawasaki_Ninja.png",
+			tags : [ "Lifestyle", "Motor" ]
+		},
+		{
+			name : "Vinilo Led Zeppelin 4",
+			owner: paco._id,
+			price : 29,
+			image : "LedZeppelin4.jpg",
+			tags : [ "Lifestyle" ]
+		},
+		{
+			name : "Sudadera Nort Face",
+			owner: kike._id,
+			price : 42,
+			image : "Nort_Face.jpg",
+			tags : [ "Lifestyle" ]
+		},
+		{
+			name : "Play Station 5",
+			owner: kike._id,
+			price : 600,
+			image : "PS5.jpg",
+			tags : [ "Lifestyle" ]
+		},
+		{
+			name : "Sony Ericson Xperia",
+			owner: paco._id,
+			price : 20,
+			image : "SE_Xperia.jpg",
+			tags : [ "Mobile" ]
+		}
+	])
+	console.log(`Insert ${insertResult.length} products.`)
 }
 
 /**
